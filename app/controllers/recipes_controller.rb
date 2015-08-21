903390991ba0a9e42257fb00b86853d603522a4f
Drew <drew.e.boardman@gmail.com>
2015-08-20 22:17:29 -0400

    Drew | Controller tests failing due to case-insensitive LIKE query in the RecipesController #index action. Need to find a better search solution.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   def index
     @recipes = if params[:keywords]
-                 Recipe.where('name ilike ?', "%#{params[:keywords]}%")
+                 Recipe.where("name ILIKE ?","%#{params[:keywords]}%")
                else
                  []
                end
